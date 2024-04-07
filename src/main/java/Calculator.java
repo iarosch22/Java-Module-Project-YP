8dfd8abc6a9a6026976893b922cac9dded2e2a2f
Vitaliy Yarash <vit.iarosch@yandex.by>
2024-04-07 13:58:04 +0300

    feat: add classes calculator, formatter, goods

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    int clients;
+
+    Calculator(int clients) {
+        this.clients = clients;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("На сколько человек необходимо разделить счет?");
+
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
